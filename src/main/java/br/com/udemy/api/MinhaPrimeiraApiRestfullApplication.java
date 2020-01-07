1209d5efd5b35f818c4e6ab1bed8db70c7cbc065
Marcio Antunes <antunes-marcio@hotmail.com>
2020-01-07 14:22:39 -0300

    Versao inicial do projeto

---
+++ src/main/java/br/com/udemy/api/MinhaPrimeiraApiRestfullApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaPrimeiraApiRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaPrimeiraApiRestfullApplication.class, args);
+	}
+
+}
