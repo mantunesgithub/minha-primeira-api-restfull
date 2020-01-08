55cc08f7d21b7d9e0495d42e3102dd76066bc07a
Marcio Antunes <antunes-marcio@hotmail.com>
2020-01-07 22:15:50 -0300

    Mais uma linha para atz note

--- src/main/java/br/com/udemy/api/MinhaPrimeiraApiRestfullApplication.java
+++ src/main/java/br/com/udemy/api/MinhaPrimeiraApiRestfullApplication.java
@@ -9,6 +9,7 @@ public class MinhaPrimeiraApiRestfullApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MinhaPrimeiraApiRestfullApplication.class, args);
 		System.out.println("Hello Spring boot!");
+		System.out.println("Linha para mais um commit");
 	}
 
 }
